fe38c11ca4be3edcf0b6d81f99612e1c8c0ec46f
Uldzh <uldis.megnis@gmail.com>
2016-10-20 07:20:37 +0300

    Initialize aws setup with terraform

---
+++ main.tf
@@ -0,0 +1,110 @@
+variable "public_key_path" {
+  description = <<DESCRIPTION
+Path to the SSH public key to be used for authentication.
+Ensure this keypair is added to your local SSH agent so provisioners can
+connect.
+Example: ~/.ssh/terraform.pub
+DESCRIPTION
+}
+
+variable "aws_access_key" {
+  description = <<DESCRIPTION
+Your AWS access_key
+DESCRIPTION
+}
+
+variable "aws_secret_key" {
+  description = <<DESCRIPTION
+Your AWS secret_key
+DESCRIPTION
+}
+
+variable "aws_instance_type" {
+  description = <<DESCRIPTION
+Your AWS instance type.
+Example: t2.micro - only one for free tier
+DESCRIPTION
+}
+
+provider "aws" {
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+  region     = "eu-west-1"
+}
+
+# Create a VPC to launch our instances into
+resource "aws_vpc" "default" {
+  cidr_block = "10.0.0.0/16"
+}
+
+# Create an internet gateway
+resource "aws_internet_gateway" "default" {
+  vpc_id = "${aws_vpc.default.id}"
+}
+
+# Grant the VPC internet access on its main route table
+resource "aws_route" "internet_access" {
+  route_table_id         = "${aws_vpc.default.main_route_table_id}"
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = "${aws_internet_gateway.default.id}"
+}
+
+# Create a subnet to launch our instance into
+resource "aws_subnet" "default" {
+  vpc_id                  = "${aws_vpc.default.id}"
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+}
+
+
+resource "aws_security_group" "default" {
+  name        = "um_ml"
+  description = "Used in the terraform"
+  vpc_id      = "${aws_vpc.default.id}"
+
+  # SSH access from anywhere
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Ipython access
+  ingress {
+    from_port   = 8888
+    to_port     = 8888
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # outbound internet access
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_key_pair" "auth" {
+  key_name   = "ML"
+  public_key = "${file(var.public_key_path)}"
+}
+
+
+resource "aws_instance" "um-ml" {
+  ami           = "ami-ed82e39e"
+  instance_type = "${var.aws_instance_type}"
+  connection {
+    user = "ubuntu"
+  }
+
+  subnet_id = "${aws_subnet.default.id}"
+
+  # THe name of our SSH keypair we created
+  key_name = "${aws_key_pair.auth.id}"
+
+  # Our Security group to allow HTTP and SSH access
+  vpc_security_group_ids = ["${aws_security_group.default.id}"]
+}
